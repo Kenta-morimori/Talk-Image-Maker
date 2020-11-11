35a794fda67115dfc60ff926a3f9ce0e964b8a39
Kenta-morimori <takemori.kenta.official@gmail.com>
2020-11-11 16:30:52 +0900

    Change url and img name
    because "add" was used by function name, too

--- Study2.py
+++ Study2.py
@@ -12,14 +12,14 @@ def add(f1, f2, out, top, left): #画像合成の関数
 
     cv2.imwrite(out, img2)
 
-add_url = 'https://user-images.githubusercontent.com/64821676/98765274-19c82580-2421-11eb-8b17-c2e6af55c0ae.jpg' #入力する画像のURL
+mat_url = 'https://user-images.githubusercontent.com/64821676/98765274-19c82580-2421-11eb-8b17-c2e6af55c0ae.jpg' #入力する画像のURL
 Temp_url = 'https://user-images.githubusercontent.com/64821676/98764622-893d1580-241f-11eb-9a37-704948ba7d42.jpeg' #テンプレ画像のURL
 
-response = requests.get(add_url)
-add_img = response.content
+response = requests.get(mat_url)
+mat_img = response.content
 response = requests.get(Temp_url)
 Temp_img = response.content
 
-add(Temp_img, add_img, 'new_img.jpg', 30, 60) #合成したい画像、合成の背景にしたい画像、出力先、合成する位置の y 座標、x 座標を指定
+add(Temp_img, mat_img, 'new_img.jpg', 30, 60) #合成したい画像、合成の背景にしたい画像、出力先、合成する位置の y 座標、x 座標を指定
 
-print('new_img.jpg') #画像の表示(add()の３つ目の引数)
+print('new_img.jpg') #画像の表示(add()の３つ目の引
