8c19d60e195cb5d7b1caaad280bfd6081b46b482
Kenta-morimori <takemori.kenta.official@gmail.com>
2020-11-11 15:51:36 +0900

    Change the way to import images

--- Study2.py
+++ Study2.py
@@ -1,16 +1,5 @@
-# from PIL import Image, ImageDraw, ImageFilter #旧案
-
-# im1 = Image.open('') #入力する画像
-# im2 = Image.open('') #テンプレ画像
-
-# back_im = im1.copy()
-# back_im.paste(im2, (100, 50)) #(左上のx座標, 左上のy座標)
-# back_im.save('data/dst/rocket_pillow_paste_pos.jpg', quality=95)
-
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-%matplotlib inline
+import requests #URLから画像を持ってくる
 
 def add(f1, f2, out, top, left): #画像合成の関数
     img1 = cv2.imread(f1)
@@ -23,12 +12,14 @@ def add(f1, f2, out, top, left): #画像合成の関数
 
     cv2.imwrite(out, img2)
 
+add_url = 'https://user-images.githubusercontent.com/64821676/98765274-19c82580-2421-11eb-8b17-c2e6af55c0ae.jpg' #入力する画像のURL
+Temp_url = 'https://user-images.githubusercontent.com/64821676/98764622-893d1580-241f-11eb-9a37-704948ba7d42.jpeg' #テンプレ画像のURL
 
+response = requests.get(add_url)
+add_img = response.content
+response = requests.get(Temp_url)
+Temp_img = response.content
 
-img1 = cv2.imread('') #入力する画像
-img2 = cv2.imread('') #テンプレ画像
+add(Temp_img, add_img, 'new_img.jpg', 30, 60) #合成したい画像、合成の背景にしたい画像、出力先、合成する位置の y 座標、x 座標を指定
 
-
-add('president.jpg', 'park.jpg', 'new.jpg', 30, 60) #合成したい画像、合成の背景にしたい画像、出力先、合成する位置の y 座標、x 座標を指定
-
-print() #画像の表示(add()の３つ目の引数)
+print('new_img.jpg') #画像の表示(add()の３つ目の引数)
