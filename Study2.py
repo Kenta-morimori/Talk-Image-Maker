f63f42a20072f9aab237e7e4c39c362bedb55fca
Kenta-morimori <takemori.kenta.official@gmail.com>
2020-11-13 20:32:34 +0900

    Change way to import and open image

--- Study2.py
+++ Study2.py
@@ -1,25 +1,25 @@
 import cv2
 import requests #URLから画像を持ってくる
+from email.mime import image
 
-def add(f1, f2, out, top, left): #画像合成の関数
-    img1 = cv2.imread(f1)
-    img2 = cv2.imread(f2)
+# def add(f1, f2, out, top, left): #画像合成の関数
+#     img1 = cv2.imread(f1)
+#     img2 = cv2.imread(f2)
 
-    img2 =cv2.resize(img2,(600,600)) #サイズ調節
+#     img2 =cv2.resize(img2,(600,600)) #サイズ調節
 
-    height, width = img1.shape[:2]
-    img2[top:height + top, left:width + left] = img1
+#     height, width = img1.shape[:2]
+#     img2[top:height + top, left:width + left] = img1
 
-    cv2.imwrite(out, img2)
+#     cv2.imwrite(out, img2)
 
 mat_url = 'https://user-images.githubusercontent.com/64821676/98765274-19c82580-2421-11eb-8b17-c2e6af55c0ae.jpg' #入力する画像のURL
 Temp_url = 'https://user-images.githubusercontent.com/64821676/98764622-893d1580-241f-11eb-9a37-704948ba7d42.jpeg' #テンプレ画像のURL
 
-response = requests.get(mat_url)
-mat_img = response.content
-response = requests.get(Temp_url)
-Temp_img = response.content
+mat_img = cv2.imread(mat_url)
+Temp_img = cv2.imread(Temp_url)
 
-add(Temp_img, mat_img, 'new_img.jpg', 30, 60) #合成したい画像、合成の背景にしたい画像、出力先、合成する位置の y 座標、x 座標を指定
-
-print('new_img.jpg') #画像の表示(add()の３つ目の引
+cv2.imread(mat_url, mat_img)
+cv2.imread(Temp_url, Temp_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
